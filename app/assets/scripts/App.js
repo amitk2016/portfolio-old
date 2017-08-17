6e5d3007a2a6f07e63d8acf4d43b5d72c31cb2ff
Amit <amit.k03377@gmail.com>
2017-08-17 14:59:47 +1200

    added reveal on scroll javacript

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll( $('.work_image'), "65%" ,"reveal-item" );
+new RevealOnScroll( $('.testimonial'), "75%" ,"reveal-item-2");
---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,46 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll{
+		constructor(els,offset,className){
+			this.itemToReveal = els;
+			this.offsetPercentage = offset;
+			this.className = className;
+			this.hideIntially();
+			this.createWaypoints();
+			
+		}
+
+		hideIntially(){
+
+			this.itemToReveal.addClass(this.className);
+			
+		}
+
+		 createWaypoints() {
+		 	var that = this;
+		    this.itemToReveal.each(function(){
+		    	var currentItem = this;
+		    	new Waypoint({
+
+				  element:currentItem,
+				  handler: function() {
+				     $(currentItem).addClass(that.className+"--is-visible");
+				  },
+       				 offset: that.offsetPercentage
+				}); 	
+		    });
+		  }
+
+
+}
+
+
+export default RevealOnScroll;
+
+
+
+
+
+
+
