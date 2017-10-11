8eb48f2e492fb4a27e69cfdd884eb22092a5fbc3
Amit <amit.k03377@gmail.com>
2017-10-11 13:46:35 +1300

    fixed alignment of mobilemenu and remove get in touch

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,57 +1,23 @@
 var gulp = require('gulp'),
-imagemin = require('gulp-imagemin'),
-del = require('del'),
-usemin = require('gulp-usemin'),
-rev = require('gulp-rev'),
-cssnano = require('gulp-cssnano'),
-uglify = require('gulp-uglify'),
-browserSync = require('browser-sync').create();
+postcss = require('gulp-postcss'),
+autoprefxer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
-gulp.task('previewDist', function() {
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "dist"
-    }
-  });
+
+
+
+
+
+gulp.task('styles',function(){
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss( [cssImport,mixins,cssvars, nested, hexrgba,autoprefxer] ))
+  .on('error',function(errorInfo){
+    console.log(errorInfo.toString());
+    this.emit('end');
+  })
+  .pipe(gulp.dest('./app/temp/styles'));
 });
-
-gulp.task('deleteDistFolder', function() {
-  return del("./dist");
-});
-
-gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
-  var pathsToCopy = [
-    './app/**/*',
-    '!./app/index.html',
-    '!./app/assets/images/**',
-    '!./app/assets/styles/**',
-    '!./app/assets/scripts/**',
-    '!./app/temp',
-    '!./app/temp/**'
-  ]
-
-  return gulp.src(pathsToCopy)
-    .pipe(gulp.dest("./dist"));
-});
-
-gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
-  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true,
-      multipass: true
-    }))
-    .pipe(gulp.dest("./dist/assets/images"));
-});
-
-gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
-  return gulp.src("./app/index.html")
-    .pipe(usemin({
-      css: [function() {return rev()}, function() {return cssnano()}],
-      js: [function() {return rev()}, function() {return uglify()}]
-    }))
-    .pipe(gulp.dest("./dist"));
-});
-
-gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
